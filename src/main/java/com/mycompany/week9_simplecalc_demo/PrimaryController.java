6af1ccf37ee4704ee9977305a002a2fe887ce013
itlabs <itlabs@WHB019_1.it.farmingdale.edu>
2021-10-28 14:49:26 -0400

    Initial commit

---
+++ src/main/java/com/mycompany/week9_simplecalc_demo/PrimaryController.java
@@ -0,0 +1,61 @@
+package com.mycompany.week9_simplecalc_demo;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class PrimaryController {
+    String opd1="0", arthOp="NULL";
+    
+    @FXML
+    private TextField tfShowOutput;
+
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+
+    @FXML
+    private void addDigit(ActionEvent event) {
+          if( tfShowOutput.getText().equals( "0" ) ) tfShowOutput.setText("");
+          
+          switch(((Button)event.getSource()).getText()){
+              case "9": tfShowOutput.appendText("9"); break;
+              case "8": tfShowOutput.appendText("8"); break;
+              case "7": tfShowOutput.appendText("7"); break;
+              case "6": tfShowOutput.appendText("6"); break;
+              case "5": tfShowOutput.appendText("5"); break;
+              case "4": tfShowOutput.appendText("4"); break;
+              case "3": tfShowOutput.appendText("3"); break;
+              case "2": tfShowOutput.appendText("2"); break;
+              case "1": tfShowOutput.appendText("1"); break;
+              case "0": tfShowOutput.appendText("0"); break;
+          }
+    }
+
+    @FXML
+    private void processArthOp(ActionEvent event) {
+         opd1= tfShowOutput.getText();
+         arthOp=((Button)event.getSource()).getText();
+          tfShowOutput.setText("");
+        
+    }
+
+    @FXML
+    private void clear(ActionEvent event) {
+    }
+
+    @FXML
+    private void equals(ActionEvent event) {
+          switch(arthOp){
+              case "+": {
+              tfShowOutput.setText( ""+
+                      (   Integer.parseInt(opd1)+Integer.parseInt(tfShowOutput.getText())    ));
+              } break;
+              case "X": { tfShowOutput.setText( ""+
+                      (   Integer.parseInt(opd1)*Integer.parseInt(tfShowOutput.getText())    ));} break; 
+        }
+        
+    }
+}
